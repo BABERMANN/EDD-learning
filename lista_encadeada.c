29468c805b91eeeaec18ce67ad12dc7908f7ffdb
Caua Matheus Habermann Pereira <cauahabermann7@gmail.com>
2026-09-20 22:25:36 -0300

    inserts the structure t_list

--- lista_encadeada.c
+++ lista_encadeada.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct _node {
+typedef struct _node { // nome intermediario pra referenciar a si mesma
     int item;
     struct _node *next;
 } t_node;
+
+typedef struct{
+    t_node * head;
+    t_node * tail;
+    int size;
+}t_list;
 
 
