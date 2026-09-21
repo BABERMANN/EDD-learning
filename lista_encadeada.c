1cd154904a2fa031767edd869aa7f69637376615
Caua Matheus Habermann Pereira <cauahabermann7@gmail.com>
2026-09-21 12:58:36 -0300

    implement remove_item() function

--- lista_encadeada.c
+++ lista_encadeada.c
@@ -1,39 +1,47 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct _node { // nome intermediario pra referenciar a si mesma
+typedef struct _node // nome intermediario pra referenciar a si mesma
+{
     int item;
     struct _node *next;
 } t_node;
 
-typedef struct{
-    t_node * head;
-    t_node * tail;
+typedef struct
+{
+    t_node *head;
+    t_node *tail;
     int size;
-}t_list;
+} t_list;
 
-t_list * create_list(){
-    t_list * lista = malloc(sizeof(t_list));
-    
+t_list *create_list()
+{
+    t_list *lista = malloc(sizeof(t_list));
+
     lista->head = NULL;
     lista->tail = NULL;
     lista->size = 0;
     return lista;
 }
 
-int is_empty(t_list * lista){
+int is_empty(t_list *lista)
+{
     return lista->size == 0;
 }
 
-int append(t_list * lista, int element){
-    t_node * new_node = malloc(sizeof(t_node));
+int append(t_list *lista, int element)
+{
+    t_node *new_node = malloc(sizeof(t_node));
     new_node->item = element; // conteudo guardado
-    new_node->next = NULL; // feito sempre no principio, pois cada nó novo nao sabe o proximo elemento
+    new_node->next = NULL;    // feito sempre no principio, pois cada nó novo nao sabe o proximo elemento
 
-    if(is_empty(lista)){
+    if (is_empty(lista))
+    {
         lista->head = new_node; // primeiro elemento sempre vai ser definido como head
-    }else{
-        lista->tail->next = new_node;    
+    }
+    else
+    {
+        lista->tail->next = new_node;
     }
     lista->tail = new_node;
     lista->size++;
@@ -41,12 +49,57 @@ int append(t_list * lista, int element){
     return 1;
 }
 
-void print_list(t_list * lista){
-t_node * aux = lista->head;
-while(aux != NULL){
-    printf("%d",aux->item);
-    aux = aux->next;
+void print_list(t_list *lista)
+{
+    t_node *aux = lista->head;
+    while (aux != NULL)
+    {
+        printf("%d", aux->item);
+        aux = aux->next;
+    }
 }
 
-}
+int remove_item(t_list *lista, t_node *node_to_remove)
+{
+    if (is_empty(lista))
+        return 0;
 
+    if (lista->size == 1 && lista->head == node_to_remove)
+    {
+        lista->head = NULL;
+        lista->tail = NULL;
+        lista->size = 0;
+        free(node_to_remove);
+        return 1;
+    }
+
+    if (lista->head == node_to_remove)
+    {
+        lista->head = lista->head->next;
+        free(node_to_remove);
+        lista->size--;
+        return 1;
+    }
+
+    t_node *aux = lista->head;
+    while (aux->next != NULL)
+    {
+        if (node_to_remove == lista->tail && aux->next == node_to_remove)
+        {
+            lista->tail = aux;
+            aux->next = NULL;
+            break;
+        }
+
+        if (aux->next == node_to_remove)
+        {
+            aux->next = aux->next->next;
+            break;
+        }
+
+        aux = aux->next;
+    }
+    free(node_to_remove);
+    lista->size--;
+    return 1;
+}
